9fb38df7cc32068c8d11e2f88f1543b2453309f5
hyojun <hjlim45678@gmail.com>
2024-08-31 16:10:26 +0900

    feat: view패키지 생성

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -3,6 +3,6 @@ package racingcar;
 public class Application {
     public static void main(String[] args) {
         System.out.println("Gogo");
-        MainController controller = new MainController()
+        MainController controller = new MainController();
     }
 }
