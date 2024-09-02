e25657162f6897b15e8cf07e7dbf1e37dad953c1
hyojun <hjlim45678@gmail.com>
2024-09-02 09:58:30 +0900

    feat: 이름 최대 글자수 검증기능 구현

--- src/main/java/racingcar/domain/CarName.java
+++ src/main/java/racingcar/domain/CarName.java
@@ -1,11 +1,23 @@
 package racingcar.domain;
 
+import racingcar.message.ErrorMessage;
+
 public class CarName {
+    private final static int MAX_NAME_LENGTH = 5;
 
     private final String name ;
 
 
     public CarName(String name) {
+        validateNameLength();
         this.name = name;
     }
+
+    private void validateNameLength() {
+        if (name.length() > MAX_NAME_LENGTH){
+            throw new IllegalArgumentException(ErrorMessage.EXCESSED_NAME_LENGTH.getMessage());
+        }
+    }
+
+
 }
--- src/main/java/racingcar/message/ErrorMessage.java
+++ src/main/java/racingcar/message/ErrorMessage.java
@@ -1,4 +1,16 @@
 package racingcar.message;
 
 public enum ErrorMessage {
+
+    EXCESSED_NAME_LENGTH("5글자 이하의 이름을 입력해주세요");
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
